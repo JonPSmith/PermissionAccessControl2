b3165b3f648bffeb9cdd8357ce907336e89cc151
Jon Smith <jon.smith@selectiveanalytics.com>
2019-06-26 14:40:51 +0100

    Turned off SSL and privacy consent

--- PermissionAccessControl2/Startup.cs
+++ PermissionAccessControl2/Startup.cs
@@ -43,7 +43,7 @@ namespace PermissionAccessControl2
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
+                options.CheckConsentNeeded = context => false; //!!!!!!!!!!!!!!!!!!!!!! Turned off
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
